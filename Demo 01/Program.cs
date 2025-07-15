cd93e8d6875663514ba7802a40abd5f6ef1569e1
martin reda <martinreda999@outlook.com>
2025-07-15 18:21:18 +0300

    02 Access Modifiers

--- Demo 01/Program.cs
+++ Demo 01/Program.cs
@@ -14,17 +14,33 @@ namespace Demo_01
              * 2-collectionn of classes , interfaces that can can used in multiable projects 
              * cannot be executable ()No running
              */
-            Prouduct p1 = new Prouduct()
-            {
-                Id = 10,
-                name = "martin",
-                price = 6000 ,
-            };
-            Console.WriteLine(p1);
-            /*
-             * ay ta8er f al class library htsm3 f al proggram 3ndy */
+            //Prouduct p1 = new Prouduct()
+            //{
+            //    Id = 10,
+            //    name = "martin",
+            //    price = 6000 ,
+            //};
+            //Console.WriteLine(p1);
+            ///*
+            // * ay ta8er f al class library htsm3 f al proggram 3ndy */
 
             #endregion
+
+            #region 02 Access Modifiers
+            /*
+             * 1- privte
+             * 2- privte protected
+             * 3- protected
+             * 4- internal
+             * 5- protected internal
+             * 6- Public
+             */
+            TypeA typeA = new TypeA();
+            typeA.x = 1; // invalid because protiction level
+            typeA.y = 2;// invalid because protiction level
+            typeA.z = 3;// valid because it Public
+            #endregion
+
         }
     }
 }
---
+++ commen/TypeA.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace commen
+{
+    public class TypeA
+    {
+        private int x;
+        internal int y;
+        public int z;
+      public TypeA()
+        {
+            x = 1;
+            y = 2;
+            z = 3;
+        }
+    }
+}
