5a153c598cefbc288f1cbcf083b7ab66fbbb6d24
martin reda <martinreda999@outlook.com>
2025-07-15 18:04:42 +0300

    01 class Library

--- Demo 01/Program.cs
+++ Demo 01/Program.cs
@@ -1,10 +1,30 @@
-﻿namespace Demo_01
+﻿using commen;
+
+namespace Demo_01
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region 01 class Library
+
+            /*class library
+             * what is class library ?? 
+             * types of projects - identity i can use it for many apps like (webapp , desc app , ai pp , mobile app).
+             * 2-collectionn of classes , interfaces that can can used in multiable projects 
+             * cannot be executable ()No running
+             */
+            Prouduct p1 = new Prouduct()
+            {
+                Id = 10,
+                name = "martin",
+                price = 6000 ,
+            };
+            Console.WriteLine(p1);
+            /*
+             * ay ta8er f al class library htsm3 f al proggram 3ndy */
+
+            #endregion
         }
     }
 }
---
+++ commen/prouduct.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace commen
+{
+    public class Prouduct
+    { 
+        public int Id { get; set; }
+        public string name { get; set; }
+        public decimal price { get; set; }
+        public override string  ToString() 
+        {
+            return $"Id : {Id} \nName : {name} \nprice : {price}"; 
+        }
+         
+
+
+    }
+}
